97bdf67310f79f40fe330403f4d6fe6455d60c49
Babloo Kumar <babloocse888@gmail.com>
2024-01-14 07:01:27 +0530

    :memo: write some express code for creating post and list posts

---
+++ posts/index.js
@@ -0,0 +1,26 @@
+const express = require('express');
+const { randomBytes } = require('crypto');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+
+const app = express();
+app.use(bodyParser.json());
+app.use(cors());
+
+const posts = {};
+app.get('/posts', (req, res) => {
+    res.send(posts);
+});
+
+app.post('/posts', (req, res) => {
+    const id = randomBytes(4).toString('hex');
+    const { title } = req.body;
+    posts[id] = {
+        id, title
+    };
+    res.status(201).send(posts[id]);
+});
+
+app.listen(5000, () => {
+    console.log("Post is listening on 5000 Port!");
+})
