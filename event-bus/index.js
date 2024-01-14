637529077fd2b43fed8cb00b1f06b200b9e531ff
Babloo Kumar <babloocse888@gmail.com>
2024-01-14 16:09:25 +0530

    :fire: creating an event-bus to making asynchronous services

---
+++ event-bus/index.js
@@ -0,0 +1,19 @@
+const express = require("express");
+const bodyParser = require('body-parser');
+const axios = require("axios");
+
+const app = express();
+app.use(bodyParser.json());
+
+app.post("/events", (req, res) => {
+    const event = req.body;
+    axios.post("http://localhost:5000/events", event);      // post service
+    axios.post("http://localhost:5001/events", event);      // comment service
+    axios.post("http://localhost:5002/events", event);      // query service
+
+    res.send({status: "OK"});
+})
+
+app.listen(5005, () => {
+    console.log("event-bus is listening on 5005 Port!");
+})
